68bce5b782c68c2ff4ff40488cac900533c34452
Rodrig Couto <rodrigo.couto.santos@hotmail.com>
2024-02-13 12:32:54 -0300

    ok

--- src/containers/home/counter.tsx
+++ src/containers/home/counter.tsx
@@ -66,7 +66,7 @@ export default function Counter() {
         </section>
     );
 
-    async function fetchData() {
+    async function fetchData(): Promise<NodeJS.Timeout> {
         const response = await query();
 
         if (
